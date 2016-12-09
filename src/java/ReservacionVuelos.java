540cf668b9ce12af20169a8338e11baff18dc13b
Tsuki <ammy_zmmn@hotmail.com>
2016-12-08 20:44:38 -0600

    Querys

--- src/java/ReservacionVuelos.java
+++ src/java/ReservacionVuelos.java
@@ -1,3 +1,13 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +19,20 @@
  * @author Marce
  */
 public class ReservacionVuelos {
-    private String aeroline;
-    private String aeropueto;
-    private String clase;
-    private String reservacion;
-    private String usuario;
-    private String vuelo;
+    private final String INSERT = "INSERT INTO tb_producto(clave, nombre, descripcion) VALUES (?, ?, ?)";
+    private final String DELETE = "DELETE FROM tb_producto WHERE id_producto = ?";
+    private final String UPDATE = "UPDATE tb_producto SET clave = ?, nombre = ?, descripcion = ? WHERE id_producto = ?";
+    private final String GETONE = "SELECT * FROM tb_producto WHERE id_producto = ?";
+    private final String GETALL = "SELECT * FROM tb_producto";
+    private final String DELETEPP = "DELETE FROM tb_productoproveedor WHERE id_producto = ?";
+    
+    public int ingresarUsuario(String nombre, String tipo, String telefono, String correo) throws SQLException {        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c= DriverManager.getConnection("jdbc:mysql://localhost/vuelos", "root", "root");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReservacionVuelos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
 }
